2b0333727163b0c96cf4246146c764c72f5a5a07
shubhamsandhanpatil <134739195+shubhamsandhanpatil@users.noreply.github.com>
2023-08-23 00:43:33 +0530

    Add files via upload

---
+++ BOL/Employee.cs
@@ -0,0 +1,73 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BOL
+{
+    public class Employee
+    {
+        private int id;
+        private string name;
+        private int age;
+        private Department department;
+        private DateOnly date;
+
+        public Employee()
+        {
+            
+        }
+
+        public Employee(int id, string name, int age, Department department, DateOnly date)
+        {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+            this.department = department;
+            this.date = date;
+        }
+
+
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+       
+
+        /*
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }*/
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Age
+        { 
+            get { return age; } 
+            set {  age = value; } 
+        }
+        public Department DEPARTMENT
+        {
+            get { return department; }
+            set { department = value; }
+        }
+        public DateOnly Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+        public override string ToString()
+        {
+            string data = String.Format("Id ={ 1},Name ={ 2},Age ={ 3},Department ={ 4},Date ={ 5}", this.id, this.name, this.age, this.department, this.date);
+            return data;
+        }
+
+    }
+    public enum Department
+    {
+        HR,MARKETING,SALES,ADMINISTRATION
+    }
+}
---
+++ DAL/DBmanager.cs
@@ -0,0 +1,59 @@
+﻿using BOL;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+
+namespace DAL;
+public class DBmanager
+{
+    public static string constring = @"server=localhost;port=3306;user=root;password=root123;database=dotnet";
+    public static List<Employee> getEmployees()
+    {
+        List<Employee> list = new List<Employee>();
+        MySqlConnection conn = new MySqlConnection();
+        conn.ConnectionString = constring;
+        try
+        {
+            conn.Open();
+            MySqlCommand cmd = conn.CreateCommand();
+            string query = "select * from emp";
+            cmd.CommandText = query;
+            MySqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                int id = int.Parse(reader["id"].ToString());
+                string name = reader["name"].ToString();
+                int age = int.Parse(reader["age"].ToString());
+                Department department = Enum.Parse<Department>(reader["department"].ToString());
+                DateOnly date = DateOnly.FromDateTime(DateTime.Parse(reader["date"].ToString()));
+                Employee emp = new Employee(id, name, age, department, date);
+                list.Add(emp);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally { conn.Close(); }
+        return list;
+
+    }
+    public static void Delete(int id)
+    {
+        MySqlConnection conn = new MySqlConnection();
+        conn.ConnectionString = constring;
+        try
+        {
+            conn.Open();
+            MySqlCommand cmd = conn.CreateCommand();
+            cmd.Connection= conn;   
+            string query = "delete from emp where id="+id;
+            cmd.CommandText = query;
+           cmd.ExecuteNonQuery();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally { conn.Close(); }
+    }
+}
